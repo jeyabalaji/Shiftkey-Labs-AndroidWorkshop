c46ed508263281a0f25ef4025dfbc881208ed5a9
JeyaBalaji <jeyabalaji_s@srmuniv.edu.in>
2016-10-19 11:45:23 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,55 @@
+package cs.dal.shifkey;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+     WebView wv;
+
+
+
+    String url="https://shiftkeyandroid.wordpress.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv=(WebView)findViewById(R.id.webView);
+
+
+
+        WebSettings webSettings = wv.getSettings();
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+        wv.getSettings().setBuiltInZoomControls(true);
+        wv.getSettings().setPluginState(WebSettings.PluginState.ON);
+        //wv.setWebChromeClient(new WebChromeClient());
+
+        wv.setWebViewClient(new myWebClient());
+
+        wv.loadUrl(url);
+    }
+
+
+
+
+    public class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+    }}
